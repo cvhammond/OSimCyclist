8a1bb5304f05511bc7e0c6e741694ffba66a7fcd
hammondjp11 <hammondjp11@gmail.com>
2020-06-13 07:45:26 -0700

    added crankset and pedal to model

--- Cyclist.cpp
+++ Cyclist.cpp
@@ -11,64 +11,112 @@ using namespace SimTK;
  */
 
 
+
+// Model Values
+const std::string NAME = "Cyclist"; //name of model in OpenSim GUI
+const std::string FILE_NAME = "cyclist.osim"; // name of file
+
+// Bike Values
+const double BBWIDTH = 0.0895; //89.5mm
+
+// Human Values
+
+void buildBB(Model* model) {
+    Ground& ground = model->updGround();
+
+    ground.attachGeometry(new Mesh("Geometry/bottombracket.stl"));
+}
+
+void buildCrankset(Model* model) {
+
+    double xx = 0.00980, yy = 0.00547, zz = 0.00829, xy = 0, xz = 0, yz = 0.00254;
+
+    double cranksetMass = 1.15;
+    Vec3 cranksetMassCenter(0.0, 0.0, 0.0199);
+    Inertia cranksetInertia = Inertia(xx, yy, zz, xy, xz, yz);
+    OpenSim::Body* crankset = new OpenSim::Body("crankset", cranksetMass,
+        cranksetMassCenter, cranksetInertia);
+
+    crankset->attachGeometry(new Mesh("Geometry/crankset.stl"));
+
+    Vec3 locationInParent(0), orientationInParent(0, Pi, 0);
+    Vec3 locationInBody(0), orientationInBody(0, Pi, 0);
+    Ground& ground = model->updGround();
+
+    PinJoint* crankAngle = new PinJoint("crankset", ground,
+        locationInParent, orientationInParent,
+        *crankset, locationInBody, orientationInBody);
+
+    double angleRange[2] = { 0.0, SimTK::Pi * 2 };
+
+    crankAngle->updCoordinate(PinJoint::Coord::RotationZ).setRange(angleRange);
+    crankAngle->updCoordinate(PinJoint::Coord::RotationZ).setName("crankAngle");
+
+    model->addBody(crankset);
+    model->addJoint(crankAngle);
+
+}
+
+void buildPedals(Model* model) {
+
+    //Left Pedal
+    double xx = 0.000459, yy = 0.0000624, zz = 0.0000198, xy = 0, xz = 0, yz = 0.0;
+    double xoffset = 0, yoffset = -0.170, zoffset = -0.146 / 2.0; //yoffset = crank length, zoffset = q-factor/2
+
+    double pedalMass = 0.106; // kg
+    Vec3 pedalMassCenter(0.0, 0.0, -0.0469);
+    Inertia pedalInertia = Inertia(xx, yy, zz, xy, xz, yz);
+
+    OpenSim::Body* leftPedal = new OpenSim::Body("leftPedal", pedalMass,
+        pedalMassCenter, pedalInertia);
+
+    leftPedal->attachGeometry(new Mesh("Geometry/leftpedal.stl"));
+
+    Vec3 locationInParent(xoffset, yoffset,zoffset), orientationInParent(0);
+    Vec3 locationInBody(0), orientationInBody(0);
+
+    PinJoint* leftPedalAngle = new PinJoint("leftPedal", model->getBodySet().get("crankset"),
+        locationInParent, orientationInParent,
+        *leftPedal, locationInBody, orientationInBody);
+
+    double angleRange[2] = { 0.0, SimTK::Pi * 2 };
+
+    leftPedalAngle->updCoordinate(PinJoint::Coord::RotationZ).setRange(angleRange);
+    leftPedalAngle->updCoordinate(PinJoint::Coord::RotationZ).setName("leftPedal");
+
+    model->addBody(leftPedal);
+    model->addJoint(leftPedalAngle);
+
+}
+
+void buildBike(Model* model) {
+
+    buildBB(model);
+    buildCrankset(model);
+    buildPedals(model);
+    
+
+}
+
+void buildHuman(Model* model) {
+
+}
+
+
+
+
 int main()
 {
     try {
-        Model osimModel;
-        osimModel.setName("Cyclist");
+        Model model;
+        model.setName(NAME);
 
-        Ground& ground = osimModel.updGround();
+        buildBike(&model);
+        buildHuman(&model);
 
-        ground.attachGeometry(new Mesh("Geometry/chainring.stl"));
-        //ground.attachGeometry(new Mesh("anchor1.vtp"));
-        //ground.attachGeometry(new Mesh("anchor2.vtp"));
-
-        double blockMass = 20.0, blockSideLength = 0.1;
-        Vec3 blockMassCenter(0);
-        Inertia blockInertia = blockMass*Inertia::brick(blockSideLength, 
-            blockSideLength, blockSideLength);
-
-        OpenSim::Body *rightCrankArm = new OpenSim::Body("crank", blockMass, 
-            blockMassCenter, blockInertia);
-
-        rightCrankArm->attachGeometry(new Mesh("Geometry/crankarm.stl"));
-
-        double halfLength = blockSideLength/2.0;
+        model.finalizeConnections();
+        model.print(FILE_NAME);
         
-        Vec3 locationInParent(0), orientationInParent(0);
-        Vec3 locationInBody(0), orientationInBody(0);
-        std::cout << locationInBody << std::endl;
-        PinJoint *crank_angle = new PinJoint("crankarm", ground, 
-            locationInParent, orientationInParent, 
-            *rightCrankArm, locationInBody, orientationInBody);
-
-        double angleRange[2] = { 0.0, SimTK::Pi * 2};
-        
-        crank_angle->updCoordinate(PinJoint::Coord::RotationZ).setRange(angleRange);
-        
-        osimModel.addBody(rightCrankArm);
-        osimModel.addJoint(crank_angle);
-
-        /*OpenSim::Body* block2 = new OpenSim::Body("block2", blockMass,
-            blockMassCenter, blockInertia);
-
-        block2->attachGeometry(new Mesh("chainring.stl"));
-
-        Vec3 locationInParent2(0), orientationInParent2(0);
-        Vec3 locationInBody2(0), orientationInBody2(0);
-        std::cout << locationInBody << std::endl;
-        FreeJoint* block2Toblock1 = new FreeJoint("block2Toblock1", *rightCrankArm,
-            locationInParent2, orientationInParent2,
-            *block2, locationInBody2, orientationInBody2);
-
-        
-
-        osimModel.addBody(block2);
-        osimModel.addJoint(block2Toblock1);*/
-
-        osimModel.finalizeConnections();
-
-        osimModel.print("Cyclist.osim");
     }
     catch (OpenSim::Exception ex)
     {
